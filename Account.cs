10b3b04fb80c802694a193a0f0febfa02fce9c35
Carlos Azofeifa <cazofeifa@arm64.local>
2022-08-16 09:06:37 -0600

    initial commit

---
+++ Account.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace New_HelloWorld_MSTest
+{
+    public class Account
+    {
+        private readonly string m_customerName;
+        private double m_balance;
+
+        public Account(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        public void Debit(double amount)
+        {
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance -= amount; // intentionally incorrect code
+        }
+
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+            
+        }
+
+/*
+        public static void Main()
+        {
+            Account ba = new Account("Mr. Bryan Walton", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+*/
+    }
+}
+
---
+++ UnitTestAccount.cs
@@ -0,0 +1,45 @@
+namespace New_HelloWorld_MSTest;
+
+[TestClass]
+public class UnitTestAccount
+{
+    [TestMethod]
+    public void TestMethod_Dummy()
+    {
+        double expected = 10.1;
+        double actual = 10;
+        Assert.AreEqual(expected, actual, 0.10, "Accounts are correctly");
+    }
+
+    [TestMethod]
+    public void TestMethod_Debit_WithValidAmount_UpdatesBalance()
+    {
+        // Arrange
+        double beginningBalance = 11.99;
+        double debitAmount = 4.55;
+        double expected = 7.44;
+        Account account = new("Mr. Bryan Walton", beginningBalance);
+
+        // Act
+        account.Debit(debitAmount);
+
+        // Assert
+        double actual = account.Balance;
+
+        Console.WriteLine(expected);
+        Console.WriteLine(actual);
+        Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+    }
+
+    [TestMethod]
+    public void TestMethod_Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+    {
+        // Arrange
+        double beginningBalance = 11.99;
+        double debitAmount = -100.00;
+        Account account = new("Mr. Bryan Walton", beginningBalance);
+
+        // Act and assert
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+    }
+}
